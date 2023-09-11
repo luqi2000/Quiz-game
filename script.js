c795a2a99d6a521e1611d1dc304e810d36b5e469
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-09-11 11:03:15 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const prompt = require("prompt-sync")(); // with this i have the possibility to write in the console
+
+let value = prompt("Enter something: "); // so in the console i can write
+console.log(value);
