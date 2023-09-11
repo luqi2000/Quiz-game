de8701ca6c32e8455d0d7a3b3f5bef654af02d8b
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-09-11 11:13:14 +0100

    messege if you answer correctly in the first question

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const prompt = require("prompt-sync")(); // with this i have the possibility to write in the console
 
-let value = prompt("Enter something: "); // so in the console i can write
-console.log(value);
+console.log("Welcome to the computer hardware quiz!");
+
+const answer1 = prompt("What is the brain of the computer? "); // so in the console i can write
+const correct_answer1 = "CPU";
+
+if (answer1 === correct_answer1) {
+  console.log("you got it correct!");
+}
